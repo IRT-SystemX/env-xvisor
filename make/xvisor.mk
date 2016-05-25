3292d0238bc14dabbd42fa7ce2fc528d91e81065
Mickaël Tansorier <mickael.tansorier@openwide.fr>
2016-05-25 12:07:10 +0200

    [MAKE] Allow to select a rootfs instead of the initramfs
    Signed-off-by: Jean Guyomarc'h <jean.guyomarch@openwide.fr>

--- make/xvisor.mk
+++ make/xvisor.mk
@@ -161,14 +161,27 @@ ifeq ($(USE_KERN_DT),1)
 	$(Q)printf "copy $(ADDRH_KERN_DT) $(ADDRH_FLASH_KERN_DT) " >> $@
 	$(Q)$(call FILE_SIZE,$(DISKB_KERN_DTB)) >> $@
 endif
+ifeq ($(USE_RFS),1)
 	$(Q)printf "copy $(ADDRH_RFS) $(ADDRH_FLASH_RFS) " >> $@
 	$(Q)$(call FILE_SIZE,$(DISKA)/$(INITRD)) >> $@
-ifeq ($(USE_KERN_DT),1)
+endif
+
+ifeq ($(USE_KERN_DT) $(USE_RFS),1 1)
 	$(Q)printf "start_linux_fdt $(ADDRH_KERN) $(ADDRH_KERN_DT) $(ADDRH_RFS) " >> $@
 	$(Q)$(call FILE_SIZE,$(DISKA)/$(INITRD)) >> $@
 else
+ifeq ($(USE_KERN_DT),1)
+	$(Q)printf "start_linux_fdt $(ADDRH_KERN) $(ADDRH_KERN_DT) " >> $@
+	$(Q)printf "\n" >> $@
+else
+ifeq ($(USE_RFS),1)
 	$(Q)printf "start_linux $(ADDRH_KERN) $(ADDRH_RFS) " >> $@
 	$(Q)$(call FILE_SIZE,$(DISKA)/$(INITRD)) >> $@
+else
+	$(Q)printf "start_linux $(ADDRH_KERN) " >> $@
+	$(Q)printf "\n" >> $@
+endif
+endif
 endif
 
 $(DISK_XVISOR_BANNER): $(XVISOR_BANNER) $(DISK_SYSTEM)
